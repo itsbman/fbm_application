d69eaed4b928c11393f43f8950fdc47c4f49c39d
itsbman <39183052+itsbman@users.noreply.github.com>
2018-12-12 19:44:03 +0900

    Add files via upload

---
+++ Assets/ShapeMesh.cs
@@ -0,0 +1,335 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShapeMesh : MonoBehaviour {
+    //public Material mtl;
+    public GameObject shapePlane;
+    private float minExtLength;
+    private Vector3[] varVertices;
+    //private bool openEnded = true;
+    MeshFilter extrudedmesh;
+    private int shapeopt = 0;
+    public int rectct;
+    public int trict;
+    public int circct;
+    private int side;
+    //MeshCollider mc;
+    public Slider height_slider;
+    public Button upButton;
+    public Button downButton;
+    public Button rightButton;
+    public Button leftButton;
+    public Button forButton;
+    public Button backButton;
+    public Button extrude_cfm;
+    //public Button extrude_cnl;
+    private Vector3 startpt;
+    private Vector3 tr;
+    private bool tranStart = false;
+    //public Text debug;
+    
+    // Use this for initialization
+    private void editingHandle(int edit_mode)
+    {
+        if (edit_mode == 0)
+        {
+            if(shapeopt == 1)
+            {
+                GameObject.Find("rectangle" + rectct.ToString()).GetComponent<ShapeMesh>().enabled = false;
+            }
+            if (shapeopt == 2)
+            {
+                GameObject.Find("circle" + circct.ToString()).GetComponent<ShapeMesh>().enabled = false;
+            }
+            if (shapeopt == 3)
+            {
+                GameObject.Find("triangle" + trict.ToString()).GetComponent<ShapeMesh>().enabled = false;
+            }
+
+        }
+        
+    }
+    private void tranHandle(int dir)
+    {
+        float deltapos = 0.5f;
+        if (dir == 1)
+        {
+            tr = new Vector3(0, 0, -deltapos);
+        }
+        if (dir == 2)
+        {
+            tr = new Vector3(0, 0, deltapos);
+        }
+        if (dir == 3)
+        {
+            tr = new Vector3(-deltapos, 0, 0);
+        }
+        if (dir == 4)
+        {
+            tr = new Vector3(deltapos, 0, 0);
+        }
+        if (dir == 5)
+        {
+            tr = new Vector3(0, deltapos, 0);
+        }
+        if (dir == 6)
+        {
+            tr = new Vector3(0, -deltapos, 0);
+        }
+        tranStart = true;
+    }
+    private void Startinit()
+    {
+        height_slider = GameObject.Find("extd_slider").GetComponent<Slider>();
+        extrude_cfm = GameObject.Find("extrude_confirm").GetComponent<Button>();
+        upButton = GameObject.Find("upButton").GetComponent<Button>();
+        downButton = GameObject.Find("downButton").GetComponent<Button>();
+        leftButton = GameObject.Find("leftButton").GetComponent<Button>();
+        rightButton = GameObject.Find("rightButton").GetComponent<Button>();
+        forButton = GameObject.Find("forButton").GetComponent<Button>();
+        backButton = GameObject.Find("backButton").GetComponent<Button>();
+        extrude_cfm.onClick.AddListener(delegate { editingHandle(0); });
+        upButton.onClick.AddListener(delegate { tranHandle(1); });
+        downButton.onClick.AddListener(delegate { tranHandle(2); });
+        leftButton.onClick.AddListener(delegate { tranHandle(3); });
+        rightButton.onClick.AddListener(delegate { tranHandle(4); });
+        forButton.onClick.AddListener(delegate { tranHandle(5); });
+        backButton.onClick.AddListener(delegate { tranHandle(6); });
+        //debug = GameObject.Find("debug").GetComponent<Text>();
+    }
+
+    public void drawMesh(Vector3[] lineVertices,int Count, int segments, int shapeOption, Vector3 startPoint) {
+        shapeopt = shapeOption;
+        //vertices = lineVertices;
+        side = segments;
+        Startinit();
+        //height_slider = GameObject.Find("extd_slider").GetComponent<Slider>();
+        minExtLength = 0.2f;
+        varVertices = lineVertices;
+        startpt = startPoint;
+        //debug.text = "activvaated";
+        if (shapeOption == 1)
+        {
+            rectct = Count;
+            shapePlane = GameObject.Find("rectangle" + Count.ToString()); //change for other shape
+            //rectObj.Add(shapePlane);
+            //mtl = Resources.Load("extrude-plane-lambert2.mat", typeof(Material)) as Material;
+            int nvertex = lineVertices.Length;
+            //float extrude_length = 5.0f;
+            Vector2[] pos = new Vector2[nvertex];
+
+            ////new///
+            shapePlane.AddComponent<MeshFilter>();
+            shapePlane.AddComponent<MeshRenderer>();
+            extrudedmesh = shapePlane.GetComponent<MeshFilter>();
+
+            
+            extrudedmesh.mesh = capPlane(lineVertices, minExtLength, segments); //extrude the plane mesh
+            MeshRenderer mcol = shapePlane.GetComponent<MeshRenderer>();
+ 
+            mcol.material = new Material(Resources.Load("extrude", typeof(Material)) as Material);
+       
+
+
+        }
+        if (shapeOption == 2)
+        {
+            circct = Count;
+            shapePlane = GameObject.Find("circle" + Count.ToString()); //change for other shape
+            shapePlane.AddComponent<MeshFilter>();
+            shapePlane.AddComponent<MeshRenderer>();
+            MeshFilter shapemesh = shapePlane.GetComponent<MeshFilter>();
+            shapemesh.sharedMesh = Build(lineVertices, minExtLength, segments, startPoint);
+            MeshRenderer mcol = shapePlane.GetComponent<MeshRenderer>();
+            mcol.material = new Material(Resources.Load("extrude", typeof(Material)) as Material);
+
+        }
+        if (shapeOption == 3)
+        {
+            trict = Count;
+            shapePlane = GameObject.Find("triangle" + Count.ToString()); //change for other shape
+            shapePlane.AddComponent<MeshFilter>();
+            shapePlane.AddComponent<MeshRenderer>();
+            MeshFilter shapemesh = shapePlane.GetComponent<MeshFilter>();
+            shapemesh.sharedMesh = Build(lineVertices, minExtLength, segments, startPoint);
+            MeshRenderer mcol = shapePlane.GetComponent<MeshRenderer>();
+            mcol.material = new Material(Resources.Load("extrude", typeof(Material)) as Material);
+
+        }
+
+
+        
+        //extrusion_state = 1;
+
+        ////
+
+
+
+    }
+    void Start()
+    {
+        
+        //rectct = 0;
+    }
+
+    Mesh capPlane(Vector3[] lineVertices, float extrudelength, int segments)
+    {
+        var capmesh = new Mesh();
+        
+        MeshCollider mc = shapePlane.AddComponent<MeshCollider>();
+        Vector2[] pos = new Vector2[lineVertices.Length];
+        pos[0] = new Vector2(lineVertices[0].x, lineVertices[0].y);
+        pos[1] = new Vector2(lineVertices[1].x, lineVertices[1].y);
+        pos[2] = new Vector2(lineVertices[2].x, lineVertices[2].y);
+        pos[3] = new Vector2(lineVertices[3].x, lineVertices[3].y);
+
+        // Create the Vector3 vertices
+        Vector3[] vertices = new Vector3[2 * segments];
+        for (int j = 0; j < segments; j++)
+        {
+            vertices[2 * j] = new Vector3(pos[j].x, pos[j].y, lineVertices[j].z);
+            vertices[2 * j + 1] = new Vector3(pos[j].x, pos[j].y, lineVertices[j].z - extrudelength);
+        }
+        var triangles = new int[] { //create triangle; need to create a formula for this
+                //side faces
+                1, 2, 0,
+                3, 2, 1,
+                4, 2, 3,
+                5, 4, 3,
+                5, 7, 4,
+                7, 6, 4,
+                1, 0, 6,
+                7, 1, 6,
+                //top and bottom faces
+                0, 2, 4,
+                4, 6, 0,
+                5, 3, 1,
+                5, 1, 7,
+            };
+        // Create the mesh
+        capmesh.Clear();
+        capmesh.vertices = vertices;
+        capmesh.triangles = triangles;
+        capmesh.RecalculateNormals();
+        capmesh.RecalculateBounds();
+        return capmesh;
+
+    }
+    Mesh Build(Vector3[] lineVertices, float extrudelength, int segments, Vector3 startPoint)
+    {
+        var mesh = new Mesh();
+
+        var vertices = new List<Vector3>();
+        //var normals = new List<Vector3>();
+        //var uvs = new List<Vector2>();
+        var triangles = new List<int>();
+        float bottom = lineVertices[0].z;
+        //float radius = Mathf.Sqrt(Mathf.Pow(endPoint.x - startPoint.x, 2) + Mathf.Pow(endPoint.y - startPoint.y, 2) + Mathf.Pow(endPoint.z - startPoint.z, 2));
+        //GenerateCap(segments + 1, top, bottom, radius, vertices, uvs, normals, true);
+        
+        var len = (segments) * 2;
+        for (int i = 0; i < segments; i++)
+        {
+            //float ratio = (float)i / (segments - 1);
+            vertices.Add(lineVertices[i]); //make this the bottom
+            //uvs.Add(new Vector2(ratio, 1f));
+            vertices.Add(lineVertices[i]-new Vector3(0f,0f,extrudelength));
+            //uvs.Add(new Vector2(ratio, 0f));
+            
+        }
+
+        for (int i = 0; i < segments; i++) //sides
+        {
+            int idx = i * 2;
+            int a = idx, b = idx + 1, c = (idx + 2) % len, d = (idx + 3) % len;
+            triangles.Add(b);
+            triangles.Add(c);
+            triangles.Add(a);
+
+            triangles.Add(c);
+            triangles.Add(b);
+            triangles.Add(d);
+
+        }
+
+        //normals.Add(new Vector3(0f, 1f, 0f));
+        vertices.Add(new Vector3(startPoint.x, startPoint.y, bottom)); //bottom center
+        //uvs.Add(new Vector2(0.5f, 1f));
+        vertices.Add(new Vector3(startPoint.x, startPoint.y, bottom-extrudelength)); // top center
+        //uvs.Add(new Vector2(0.5f, 0f));
+        //normals.Add(new Vector3(0f, -1f, 0f));
+        
+
+        //var offset = len;
+        var ib = vertices.Count - 2;
+        var it = vertices.Count - 1;
+
+        //upper
+        for (int i = 0; i < len; i += 2)
+        {
+            triangles.Add(ib);
+            triangles.Add(i);
+            triangles.Add((i + 2) % len);
+            
+            
+        }
+
+        //down
+        for (int i = 1; i < len; i += 2)
+        {
+            triangles.Add(it);
+            triangles.Add((i + 2) % len);
+            triangles.Add(i);
+            
+            Debug.Log("bottomtriangle");
+            Debug.Log(ib);
+            Debug.Log(i);
+            Debug.Log((i + 2) % len);
+            
+        }
+
+        mesh.Clear();
+
+        mesh.vertices = vertices.ToArray();
+        //mesh.uv = uvs.ToArray();
+        //mesh.normals = normals.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+        return mesh;
+    }
+    private void Update()
+    {
+        if (shapeopt == 1)
+        {
+            
+            minExtLength = 0.2f + height_slider.value * 5.8f;
+            shapePlane.GetComponent<MeshFilter>().mesh = capPlane(varVertices, minExtLength, side);
+            //mc.sharedMesh = extrudedmesh.sharedMesh;
+        }
+        if (shapeopt == 2)
+        {
+            minExtLength = 0.2f + height_slider.value * 5.8f;
+            shapePlane.GetComponent<MeshFilter>().mesh = Build(varVertices, minExtLength, side, startpt);
+            //mc.sharedMesh = extrudedmesh.sharedMesh;
+        }
+        if (shapeopt == 3)
+        {
+            minExtLength = 0.2f + height_slider.value * 5.8f;
+            shapePlane.GetComponent<MeshFilter>().mesh = Build(varVertices, minExtLength, side, startpt);
+            //mc.sharedMesh = extrudedmesh.sharedMesh;
+        }
+        if (tranStart)
+        {
+            shapePlane.transform.position += tr;
+            tranStart = false;
+        }
+        
+        
+    }
+
+
+}
---
+++ Assets/createShape.cs
@@ -0,0 +1,273 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class createShape : MonoBehaviour
+{
+    bool drawStart = false;
+    private ShapeMesh createMesh;
+    public Vector3 startVertex;
+    public Vector3 startPoint;
+    public Vector3 endPoint;
+    public Vector3 curVertex;
+    public Material mtl;
+    int numLine;
+    private int active = 0;
+    private int shapeOption = 0;
+    private int rectCount = 0;
+    private int circleCount = 0;
+    private int triangleCount = 0;
+    LineRenderer lr;
+    Vector3[] lineVertices;
+    public Camera cam;
+    private float zinit;
+    public Text par1;
+    public Text par2;
+  
+
+
+    public void shapeHandle(int shape)
+    {
+        shapeOption = shape;
+        par1 = GameObject.Find("parameter1").GetComponent<Text>();
+        par2 = GameObject.Find("parameter2").GetComponent<Text>();
+        lr.material = new Material(Shader.Find("Diffuse"));
+        lr.startWidth = 0.05f;
+        lr.endWidth = 0.05f;
+        lr.loop = true;
+        if (shapeOption == 1) //rectangle
+        {
+            numLine = 4;
+            lr.positionCount = numLine;
+            
+
+            lineVertices = new Vector3[numLine];
+   
+        }
+        if (shapeOption == 2) //circle
+        {
+            numLine = 20;
+            lr.positionCount = numLine;
+          
+            lineVertices = new Vector3[numLine];
+        }
+        if (shapeOption == 3) //triangle
+        {
+            numLine = 3;
+            lr.positionCount = numLine;
+           
+            lineVertices = new Vector3[numLine];
+        }
+    }
+    public void activate(int state)
+    {
+        //createdshapes = GameObject.FindGameObjectsWithTag("shape");
+        //Debug.Log("here");
+        active = state;
+        //Debug.Log(active);
+        if (active == 0)
+        {
+            lr.positionCount = 0;
+
+        }
+        if (active == 1)
+        {
+
+        }
+        if (active == 2) //for confirmation
+        {
+            
+            par1.text = string.Empty;
+            par2.text = string.Empty;
+            if (shapeOption == 1)
+            {
+                
+                if (lineVertices[0] != lineVertices[2])
+                {
+                    Debug.Log("here");
+                    rectCount = rectCount + 1;
+                    GameObject shapePlane = new GameObject();
+                    shapePlane.name = "rectangle" + rectCount.ToString();
+                    //shapePlane.tag = "shape";
+                    shapePlane.AddComponent<ShapeMesh>();
+                    //shapePlane.AddComponent<PlaneExtrude>().extrude_state(1, lineVertices, rectCount);
+                    //shapePlane.AddComponent<ShapeMeshProperties>();
+                    shapePlane.AddComponent<LineRenderer>();
+                    LineRenderer shapeLine = shapePlane.GetComponent<LineRenderer>();
+                    shapeLine.positionCount = numLine;
+                    //shapeLine.material = new Material(Shader.Find("Particles/Additive"));
+                    shapeLine.material.color = Color.white;
+                    shapeLine.startWidth = 0.05f;
+                    shapeLine.endWidth = 0.05f;
+                    shapeLine.loop = true;
+                    shapeLine.enabled = false;
+                    shapePlane.GetComponent<ShapeMesh>().drawMesh(lineVertices, rectCount, numLine, shapeOption, startVertex);
+                }
+                else
+                {
+                    lineVertices[0] = new Vector3(0, 0, 0);
+                    lineVertices[1] = new Vector3(0, 0, 0);
+                    lineVertices[2] = new Vector3(0, 0, 0);
+                    lineVertices[3] = new Vector3(0, 0, 0);
+
+                }
+            }
+            if (shapeOption == 2)
+            {
+                //something
+                circleCount = circleCount + 1;
+                GameObject shapePlane = new GameObject();
+                shapePlane.name = "circle" + circleCount.ToString();
+                shapePlane.AddComponent<ShapeMesh>();
+                shapePlane.AddComponent<LineRenderer>();
+                LineRenderer shapeLine = shapePlane.GetComponent<LineRenderer>();
+                shapeLine.positionCount = numLine;
+                shapeLine.material.color = Color.white;
+                //shapePlane.tag = "shape";
+                shapeLine.startWidth = 1f;
+                shapeLine.endWidth = 1f;
+                shapeLine.loop = true;
+                shapeLine.enabled = false;
+                shapePlane.GetComponent<ShapeMesh>().drawMesh(lineVertices, circleCount, numLine, shapeOption, startPoint);
+            }
+            if (shapeOption == 3)
+            {
+                //something
+                triangleCount = triangleCount + 1;
+                GameObject shapePlane = new GameObject();
+                shapePlane.name = "triangle" + triangleCount.ToString();
+                shapePlane.AddComponent<ShapeMesh>();
+                //shapePlane.tag = "shape";
+
+                shapePlane.AddComponent<LineRenderer>();
+                LineRenderer shapeLine = shapePlane.GetComponent<LineRenderer>();
+                shapeLine.positionCount = numLine;
+                shapeLine.material.color = Color.white;
+                shapeLine.startWidth = 1f;
+                shapeLine.endWidth = 1f;
+                shapeLine.loop = true;
+                shapeLine.enabled = false;
+                shapePlane.GetComponent<ShapeMesh>().drawMesh(lineVertices, triangleCount, numLine, shapeOption, startPoint);
+            }
+            shapeOption = 0;
+            
+
+        }
+
+    }
+
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+        cam = Camera.main;
+        zinit = cam.nearClipPlane+5;
+        
+
+    }
+    private bool IsPointerOverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (active == 1 && shapeOption != 0)
+        {
+            
+
+            if (shapeOption == 1) //rectangle
+            {
+
+                
+                if (Input.GetMouseButtonDown(0) && !drawStart && !IsPointerOverUIObject())
+                //if (Input.GetTouch(0).phase == TouchPhase.Began && !drawStart && !EventSystem.current.IsPointerOverGameObject(0))
+                {
+                    //par1.text = "triggered";
+                    drawStart = true;
+                    Vector2 mousePos = Input.mousePosition;
+                    //Vector2 mousePos = Input.GetTouch(0).position;
+                    startVertex = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, zinit));
+                    lineVertices[0] = startVertex;
+                }
+                //Debug.Log(drawStart);
+                
+                if (Input.GetMouseButton(0) && drawStart)
+                //if(Input.touchCount > 0 && drawStart)
+                {
+                    //bool test = EventSystem.current.IsPointerOverGameObject(0);
+                    //par1.text = test.ToString();
+                    Vector2 mousePos = Input.mousePosition;
+                    //bool test2 = Input.GetMouseButton(0);
+                    //par2.text = mousePos.x.ToString();
+                    //Vector2 mousePos = Input.GetTouch(0).position;
+
+                    curVertex = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, zinit));
+                    lineVertices[1] = new Vector3(startVertex.x, curVertex.y, curVertex.z);
+                    lineVertices[2] = new Vector3(curVertex.x, curVertex.y, curVertex.z);
+                    lineVertices[3] = new Vector3(curVertex.x, startVertex.y, curVertex.z);
+                    //lineVertices[4] = startVertex;
+                    float length = Mathf.Abs(curVertex.x - startVertex.x);
+                    float width = Mathf.Abs(curVertex.y - startVertex.y);
+                    par1.text = "L:"+length.ToString();
+                    par2.text = "W:"+width.ToString();
+                    //Debug.Log(lineVertices[0]);
+                    //Debug.Log(lineVertices[2]);
+                    lr.SetPositions(lineVertices);
+                }
+                if (Input.GetMouseButtonUp(0))
+                {
+                    drawStart = false;
+                }
+            }
+            //Debug.Log(startVertex);
+            if (shapeOption == 2 || shapeOption ==3) //circle or triangle
+            {
+                //Debug.Log(drawStart);
+                if (Input.GetMouseButtonDown(0) && !drawStart && !IsPointerOverUIObject())
+                {
+                    drawStart = true;
+                    Vector2 mousePos = Input.mousePosition;
+                    startPoint = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, zinit ));
+                    //lineVertices[0] = startVertex;
+                }
+                if (Input.GetMouseButton(0) && drawStart)
+                {
+
+                    Vector2 mousePos = Input.mousePosition;
+                    endPoint = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, zinit ));
+                    Vector3 radius = new Vector3(endPoint.x - startPoint.x, endPoint.y - startPoint.y, endPoint.z - startPoint.z);
+                    for (int i = 0; i < numLine ; i++ ){
+                        float ratio = (float)i / (numLine);
+                        float rad = ratio * Mathf.PI * 2f;
+                        float cos = Mathf.Cos(rad), sin = Mathf.Sin(rad);
+                        float x = cos * radius.x - sin * radius.y, y = sin * radius.x + cos * radius.y; //only works in x-y plane
+                        lineVertices[i] = new Vector3(startPoint.x+x, startPoint.y + y , endPoint.z); ;
+                    }
+                    if (shapeOption == 2)
+                    {
+                        float absrad = Mathf.Sqrt(radius.x * radius.x + radius.y * radius.y);
+                        par2.text = "R:" + absrad.ToString();
+                    }
+                    if (shapeOption == 3)
+                    {
+                        float absrad = Mathf.Sqrt(lineVertices[0].x * lineVertices[0].x + lineVertices[0].y * lineVertices[0].y);
+                        par2.text = "L:" + absrad.ToString();
+                    }
+
+                    lr.SetPositions(lineVertices);
+                }
+                if (Input.GetMouseButtonUp(0))
+                {
+                    drawStart = false;
+                }
+            }
+        }
+    }
+}
